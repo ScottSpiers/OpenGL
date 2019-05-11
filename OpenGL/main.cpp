2fd21b1eb816baa8d67ba171393272a009344c76
ScottSpiers <scott.m.spiers@gmail.com>
2019-05-11 18:10:35 +0100

    Simple input processing

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,8 +1,44 @@
 
 #include <SDL.h>
+#include <glew.h>
+
+#include <iostream>
+
+void ProcessInput(); 
+
+bool shouldExit = false;
 
 int main(int args, char** argv)
 {
+
 	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window* m_window;
+	int screenWidth = 1920;
+	int screenHeight = 1080;
+
+	SDL_CreateWindow("Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_OPENGL);
+
+	while (!shouldExit)
+	{
+		ProcessInput();
+	}
+
 	return 0;
+}
+
+void ProcessInput()
+{
+	SDL_Event e;
+
+	while (SDL_PollEvent(&e))
+	{
+		switch (e.type)
+		{
+		case SDL_QUIT: shouldExit = true;
+		default:
+			break;
+		}
+	}
+
 }
